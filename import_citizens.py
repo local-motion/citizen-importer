481d798cd144e93b8ec7cb0647a55a1586c9de55
Errrrk <43416374+errrrk@users.noreply.github.com>
2018-09-30 17:36:33 +0200

    Install sentry-kubernetes to send cluster errors to Sentry
    Installing the official incubator/sentry helm chart refuses to
    start. Redis doesn't want to boot for whatever reason.
    
    To avoid getting stuck, I'm suggesting that whoever wants to use Sentry
    during development, creates its own Project in Sentry and use that to
    develop.
    
    Sentry.io comes with 10k errors and 1 user for free. That should be
    enough for a month of development...
    
    local-motion/product#14

--- import_citizens.py
+++ import_citizens.py
@@ -4,17 +4,27 @@ from pathlib import Path
 import requests
 import sys
 
-if len(sys.argv) != 3:
-    print("Usage: python {} http://localhost:8082/citizens samples/3_citizens.csv".format(Path(__file__).name))
+from raven import Client
+
+if len(sys.argv) != 4:
+    print("Usage: python {} http://localhost:8082/citizens samples/3_citizens.csv <sentry-dsn>".format(Path(__file__).name))
     sys.exit(1)
 
 target_endpoint = sys.argv[1]
 source = sys.argv[2]
+sentry_dsn = sys.argv[3]
+
+print("Connecting to Sentry using DSN {}".format(sentry_dsn))
+sentry = Client(sentry_dsn)
 
 with open(source, 'rt') as csv_file:
     reader = csv.DictReader(csv_file)
 
     for row in reader:
         print(row)
-        r = requests.post(target_endpoint, data=row)
-        print(r)
+        # noinspection PyBroadException
+        try:
+            r = requests.post(target_endpoint, data=row)
+            print(r)
+        except BaseException:
+            sentry.captureException()
