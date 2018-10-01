652010f77b6ca00ce955d71aaf4bb062c6c14860
Errrrk <43416374+errrrk@users.noreply.github.com>
2018-10-01 14:51:50 +0200

    Dockerized import_citizens.py and add to Travis CI

--- src/import_citizens.py
+++ src/import_citizens.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from pathlib import Path
 
 import requests
@@ -6,15 +7,21 @@ import sys
 
 from raven import Client
 
+imports_directory = os.environ['CSV_SOURCE_DIRECTORY']
+print("CSV source directory: {}".format(imports_directory))
+
 if len(sys.argv) != 4:
     print("Usage: python {} http://localhost:8082/citizens samples/3_citizens.csv <sentry-dsn>".format(Path(__file__).name))
     sys.exit(1)
 
 target_endpoint = sys.argv[1]
-source = sys.argv[2]
+source = imports_directory + "/" + sys.argv[2]
 sentry_dsn = sys.argv[3]
 
+print("Endpoint: {}".format(target_endpoint))
+print("CSV source file: {}".format(source))
 print("Connecting to Sentry using DSN {}".format(sentry_dsn))
+
 sentry = Client(sentry_dsn)
 
 with open(source, 'rt') as csv_file:
